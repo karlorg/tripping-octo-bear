fb2b3d5c5df6b34b7510596a8a7a9854cbc7fb98
karlorg <karlorg@users.noreply.github.com>
2014-11-22 13:26:58 +0000

    Jon Blow changed my mind about that refactor

--- src/Index.hx
+++ src/Index.hx
@@ -14,7 +14,7 @@ class Index {
 	public static function main() {
 		try {
 			// looks for a method of WebApi called 'doXxx' where 'xxx' is
-			// the requested page name
+			// the requested page
 			Dispatch.run(Web.getURI(), Web.getParams(), new WebApi());
 		} catch (e : DispatchError) {
 			Web.redirect("game");
@@ -37,11 +37,57 @@ class WebApi {
 	{
 		initDb();
 
-		tryPlayMove(args.playNo, args.playAt);
+		playMove(args.playNo, args.playAt);
 
-		var goban = getGobanForGame(0);
-		var gobanHtml = getHtmlForGoban(goban, 0);
+		var gameNo = 0;
+		var moves = GoMove.manager.search($gameId == gameNo);
 
+		/* build a 2d array of `null` for empty spaces, numbers for stones (by
+		 * default 0 is black, 1 is white)
+		 */
+		var goban : Array<Array<Null<Int>>>;
+		{
+			goban = [for (y in 0...Index.boardSize)
+					[for (x in 0...Index.boardSize) null]];
+			for (move in moves) {
+				goban[move.row][move.col] = move.color;
+			}
+		}
+
+		/* build the html for this goban */
+		var gobanHtml : String;
+		{
+			var moveCount = moves.length;
+			var gobanBuf = new StringBuf();
+			gobanBuf.add("<table id='goban' class='goban'>");
+			for (y in 0...Index.boardSize) {
+				gobanBuf.add("<tr>");
+				for (x in 0...Index.boardSize) {
+					gobanBuf.add("<td>");
+					switch (goban[y][x]) {
+						case null:
+							gobanBuf.add('<a href="game?playAt=');
+							gobanBuf.add(String.fromCharCode("a".code + x));
+							gobanBuf.add(String.fromCharCode("a".code + y));
+							gobanBuf.add('&playNo=' + moveCount);
+							gobanBuf.add('">');
+							gobanBuf.add('<img src="images/goban/e.gif" />');
+							gobanBuf.add('</a>');
+						case 0:
+							gobanBuf.add('<img src="images/goban/b.gif" />');
+						case 1:
+							gobanBuf.add('<img src="images/goban/w.gif" />');
+						default:
+					}
+					gobanBuf.add("</td");
+				}
+				gobanBuf.add("</tr>");
+			}
+			gobanBuf.add("</table>");
+			gobanHtml = gobanBuf.toString();
+		}
+
+		/* write the page */
 		var template = new haxe.Template(haxe.Resource.getString("game"));
 		var output = template.execute({ goban: gobanHtml });
 		Lib.print(output);
@@ -49,11 +95,9 @@ class WebApi {
 
 	private static var dbReady = false;
 	private static inline function initDb() : Void {
-		if (!dbReady) {
-			reallyInitDb();
+		if (dbReady) {
+			return;
 		}
-	}
-	private static function reallyInitDb() : Void {
 		sys.db.Manager.initialize();
 		sys.db.Manager.cnx = sys.db.Sqlite.open(Index.dbFilename);
 		if (!sys.db.TableCreate.exists(GoMove.manager)) {
@@ -62,6 +106,9 @@ class WebApi {
 		dbReady = true;
 	}
 
+	/*
+	 * Add a move into the GoMove db table.
+	 */
 	private static function addMove(
 		game: Int, moveNo: Int, color: Int, col: Int, row: Int) : Void {
 		var move = new GoMove();
@@ -73,15 +120,16 @@ class WebApi {
 		move.insert();
 	}
 
-	private static inline function tryPlayMove(
+	/*
+	 * Check and play a move specified as request parameters.
+	 */
+	private static function playMove(
 		moveNo : Null<Int>, coordsStr : Null<String>) : Void
 	{
-		if (moveNo != null && coordsStr != null) {
-			playMove(moveNo, coordsStr);
+		if (moveNo == null || coordsStr == null) {
+			return;
 		}
-	}
-	private static function playMove(moveNo : Int, coordsStr : String) : Void
-	{
+
 		var lastMoveList : List<GoMove> =
 			GoMove.manager.search($gameId == 0,
 				{ orderBy : -moveNo, limit : 1 });
@@ -114,59 +162,6 @@ class WebApi {
 		}
 	}
 
-	/*
-	 * builds an array of `null` for empty spaces, numbers for stones (by
-	 * default 0 is black, 1 is white)
-	 */
-	private static function getGobanForGame(gameNo : Int)
-		: Array<Array<Null<Int>>>
-	{
-		// TODO: unit testing should confirm that this returns the
-		// correct array size for the game
-		var goban = [for (y in 0...Index.boardSize)
-				[for (x in 0...Index.boardSize) null]];
-		var moves = GoMove.manager.search($gameId == gameNo);
-		for (move in moves) {
-			goban[move.row][move.col] = move.color;
-		}
-		return goban;
-	}
-
-	private static function getHtmlForGoban(
-		goban : Array<Array<Null<Int>>>,
-		gameNo : Int)
-		: String
-	{
-		var moveCount = GoMove.manager.count($gameId == gameNo);
-		var gobanBuf = new StringBuf();
-		gobanBuf.add("<table id='goban' class='goban'>");
-		for (y in 0...19) {
-			gobanBuf.add("<tr>");
-			for (x in 0...19) {
-				gobanBuf.add("<td>");
-				switch (goban[y][x]) {
-					case null:
-						gobanBuf.add('<a href="game?playAt=');
-						gobanBuf.add(String.fromCharCode("a".code + x));
-						gobanBuf.add(String.fromCharCode("a".code + y));
-						gobanBuf.add('&playNo=' + moveCount);
-						gobanBuf.add('">');
-						gobanBuf.add('<img src="images/goban/e.gif" />');
-						gobanBuf.add('</a>');
-					case 0:
-						gobanBuf.add('<img src="images/goban/b.gif" />');
-					case 1:
-						gobanBuf.add('<img src="images/goban/w.gif" />');
-					default:
-				}
-				gobanBuf.add("</td");
-			}
-			gobanBuf.add("</tr>");
-		}
-		gobanBuf.add("</table>");
-
-		return gobanBuf.toString();
-	}
 
 }
 
@@ -180,7 +175,8 @@ class WebApi {
 	public var id : SId;
 	#end
 	public var gameId : SInt;
-	public var moveNo : SInt;
+	public var moveNo : SInt; // SUInt causes problems with comparison in
+	// PHP, when comparing against standard Ints
 	public var color : STinyUInt;
 	public var col : STinyUInt;
 	public var row : STinyUInt;
